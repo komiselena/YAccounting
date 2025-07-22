04ee677b8f264e9c74fd169690ec743d696ff787
komiselena <komiselen@yandex.ru>
2025-07-22 18:52:00 +0300

    lottie

---
+++ YAccounting/UI/LaunchLottieView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchLottieView.swift
+//  YAccounting
+//
+//  Created by Mac on 22.07.2025.
+//
+
+import SwiftUI
+
+struct LaunchLottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchLottieView()
+}
